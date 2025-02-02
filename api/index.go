ee2dbf623faf4541dfbe3e6b39cdb52f20df953f
MBSA-INFINITY <mbsaiaditya@gmail.com>
2025-02-02 20:42:39 +0530

    mbsa

--- api/index.go
+++ api/index.go
@@ -1,10 +1,29 @@
 package handler
 
 import (
-	"fmt"
+	"html/template"
 	"net/http"
+	"path"
+
+	"github.com/gorilla/mux"
 )
 
-func Handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "<h1>Hello from Go!</h1>")
+func Handler(w http.ResponseWriter, req *http.Request) {
+	r := mux.NewRouter()
+	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("Content-Type", "text/html; charset=UTF-8")
+		fp := path.Join("templates", "index.html")
+
+		templ, err := template.ParseFiles(fp)
+
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+
+		if err := templ.Execute(w, nil); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+	})
+	http.ListenAndServe(":8080", r)
+
 }
