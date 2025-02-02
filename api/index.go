53c3daff972fb8ced248e982edf791f57274f3fe
MBSA-INFINITY <mbsaiaditya@gmail.com>
2025-02-02 20:48:08 +0530

    SFEDWFERWF

--- api/index.go
+++ api/index.go
@@ -1,9 +1,7 @@
 package handler
 
 import (
-	"html/template"
 	"net/http"
-	"path"
 
 	"github.com/gorilla/mux"
 )
@@ -11,18 +9,7 @@ import (
 func Handler(w http.ResponseWriter, req *http.Request) {
 	r := mux.NewRouter()
 	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Add("Content-Type", "text/html; charset=UTF-8")
-		fp := path.Join("templates", "index.html")
-
-		templ, err := template.ParseFiles(fp)
-
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-		}
-
-		if err := templ.Execute(w, nil); err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-		}
+		w.Write([]byte("<h1>Welcome to API by LearnCodeOnline</h1>"))
 	})
 	http.ListenAndServe(":8080", r)
 
