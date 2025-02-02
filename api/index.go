07c5ef68cbfd146fcbd628a1ee38e38ae758a89a
MBSA-INFINITY <mbsaiaditya@gmail.com>
2025-02-02 21:05:56 +0530

    trying templates

--- api/index.go
+++ api/index.go
@@ -1,7 +1,9 @@
 package handler
 
 import (
+	"html/template"
 	"net/http"
+	"path"
 
 	. "github.com/tbxark/g4vercel"
 )
@@ -10,20 +12,20 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 	server := New()
 
 	server.GET("/", func(context *Context) {
-		context.JSON(200, H{
-			"message": "hello go from vercel !!!!",
-		})
-		// fp := path.Join("templates", "index.html")
+		// context.JSON(200, H{
+		// 	"message": "hello go from vercel !!!!",
+		// })
+		fp := path.Join("templates", "index.html")
 
-		// templ, err := template.ParseFiles(fp)
+		templ, err := template.ParseFiles(fp)
 
-		// if err != nil {
-		// 	http.Error(w, err.Error(), http.StatusInternalServerError)
-		// }
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
 
-		// if err := templ.Execute(w, nil); err != nil {
-		// 	http.Error(w, err.Error(), http.StatusInternalServerError)
-		// }
+		if err := templ.Execute(w, nil); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
 	})
 	server.Handle(w, r)
 }
